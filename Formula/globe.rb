36a9603668aa4ac248ca30e055cc9cf9db408060
afawe3331 <bichpul@naver.com>
2025-11-27 16:23:50 +0900

    Update globe.rb

--- Formula/globe.rb
+++ Formula/globe.rb
@@ -1,18 +1,17 @@
 class Globe < Formula
-  desc "ASCII globe for the terminal"
+  desc "Interactive ASCII globe generator"
   homepage "https://github.com/adamsky/globe"
   url "https://github.com/adamsky/globe/archive/refs/heads/master.zip"
-  version "latest"
+  version "0.2.1"
   license "GPL-3.0-or-later"
 
-  depends_on "gcc" => :build
+  depends_on "rust" => :build
 
   def install
-    system ENV.cc, "globe.c", "-o", "globe", "-lm"
-    bin.install "globe"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/globe --help", 1)
+    assert_match "Usage", shell_output("#{bin}/globe -h")
   end
 end
